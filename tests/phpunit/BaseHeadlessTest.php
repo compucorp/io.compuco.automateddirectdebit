e9ebf914861fb5e2da6ee02d765895cf70be845a
Omar Hussein <opetmar91@gmail.com>
2023-03-31 21:36:31 +0300

    CIWEMB-253: Add tests workflow

---
+++ tests/phpunit/BaseHeadlessTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use Civi\Test;
+use Civi\Test\HeadlessInterface;
+use Civi\Test\TransactionalInterface;
+
+/**
+ * An abstract BaseHeadlessTest class.
+ */
+abstract class BaseHeadlessTest extends PHPUnit\Framework\TestCase implements HeadlessInterface, TransactionalInterface, Test\HookInterface {
+
+  /**
+   * Sets up Headless, use stock schema,, install extensions.
+   */
+  public function setUpHeadless() {
+    return Test::headless()
+      ->install('uk.co.compucorp.membershipextras')
+      ->installMe(__DIR__)
+      ->apply();
+  }
+
+}
